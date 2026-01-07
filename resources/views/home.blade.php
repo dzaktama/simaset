9d3ff64d46633991777e72d17813fd21c1864dea
dzaktama <124230024@student.upnyk.ac.id>
2026-01-07 10:44:20 +0700

    mengubah judul halaman beranda

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="text-3xl font-bold mb-6">Daftar anak Magang</h1>
+    <h1 class="text-3xl font-bold mb-6">Daftar Pengguna</h1>
 
     <a href="/users/create" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mb-4">
         + Tambah User Baru
